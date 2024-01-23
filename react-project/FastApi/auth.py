208c80fc9e2ce286594b99ea477920c96dfe41d7
DawidBudzynsky <dawidbudzynski18@gmail.com>
2024-01-23 21:05:06 +0100

    running on server

--- react-project/FastApi/auth.py
+++ react-project/FastApi/auth.py
@@ -91,7 +91,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
 #
 
 
-@router.post("/token/")
+@router.post("/token")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
 ):
--- react-project/FastApi/main.py
+++ react-project/FastApi/main.py
@@ -1,24 +1,46 @@
 from fastapi import FastAPI, HTTPException, Depends, status
 from typing import Annotated
 from datetime import datetime, timedelta
+from fastapi.routing import APIRoute
 from starlette.status import HTTP_200_OK
 import models
 from database import engine, SessionLocal
-from sqlalchemy.orm import Session, declarative_base
+from sqlalchemy.orm import Session
 import schemas
 from fastapi.middleware.cors import CORSMiddleware
 import auth
 from database import get_db
 from schemas import Groupe
-from typing import Union
 from sqlalchemy.orm.strategy_options import joinedload
 from sqlalchemy.orm.exc import StaleDataError
-from sqlalchemy import func
+
+# from fastapi.staticfiles import StaticFiles
+from fastapi.routing import APIRouter
+from starlette.routing import Router
+import api
+from fastapi.staticfiles import StaticFiles
+from fastapi.requests import Request
+from starlette.responses import RedirectResponse
+
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+from typing import Callable, Awaitable
+from fastapi.responses import Response
+from starlette.routing import Mount
 
 app = FastAPI()
-app.include_router(auth.router)
 
-origins = ["http://localhost:3000"]
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "http://192.168.1.103:3000",
+    "http://192.168.63.230:3000",
+    "http://0.0.0.0:80",
+    "http://0.0.0.0/",
+]
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,353 +50,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 models.Base.metadata.create_all(bind=engine)
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(auth.get_current_user)]
 
 
-def ensure_user_group(
-    current_user: user_dependency, required_group: Groupe = Groupe.Admin
-):
-    user_group = Groupe(current_user["groupe"])
-    if user_group != required_group.name:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You do not have access to this resource",
-        )
-    return current_user
+app.mount("/static", StaticFiles(directory="static/static", html=True), name="static")
+
+templates = Jinja2Templates(directory="static")
+
+app.include_router(auth.router)
+app.include_router(api.router)
 
 
-@app.post(
-    "/users",
-    status_code=status.HTTP_201_CREATED,
-    response_model=schemas.User,
-)
-async def create_user(user: schemas.UserBase, db: db_dependency):
-    db_user = models.User(**user.model_dump())
-    db.add(db_user)
-    db.commit()
-    return db_user
-
-
-@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
-async def read_user(user_id: int, db: db_dependency):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
-
-
-@app.get(
-    "/users",
-    status_code=status.HTTP_200_OK,
-    dependencies=[Depends(ensure_user_group)],
-)
-async def read_all_users(db: db_dependency, skip: int = 0, limit: int = 100):
-    users = db.query(models.User).offset(skip).limit(limit).all()
-    return users
-
-
-@app.patch("/users/{user_id}", response_model=schemas.User)
-async def update_user(user_id: int, user: schemas.UserEdit, db: db_dependency):
-    db_user = db.get(models.User, user_id)
-    if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    user_data = user.model_dump(exclude_unset=True)
-    for key, value in user_data.items():
-        setattr(db_user, key, value)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-
-@app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: int, db: db_dependency):
-    # Check if the user exists
-    db_user = db.query(models.User).filter(models.User.id == user_id).first()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    visits_to_update = (
-        db.query(models.Visit).filter(models.Visit.user_id == user_id).all()
-    )
-    for visit in visits_to_update:
-        visit.user_id = None
-        visit.is_reserved = False
-    try:
-        db.commit()
-        db.delete(db_user)
-        db.commit()
-        return None
-    except StaleDataError:
-        raise HTTPException(
-            status_code=409, detail="Data has changed. Please refresh and try again."
-        )
-
-
-@app.get("/current_user", status_code=status.HTTP_200_OK)
-async def current_user(user: user_dependency, db: db_dependency):
-    if user is None:
-        raise HTTPException(status_code=401, detail="Authentication Failed")
-    if user["groupe"] == "Doctor":
-        db_user = db.query(models.Doctor).get(user["id"])
-    else:
-        db_user = db.query(models.User).get(user["id"])
-    return db_user
-
-
-@app.post(
-    "/doctors", status_code=status.HTTP_201_CREATED, response_model=schemas.Doctor
-)
-async def create_doctor(doctor: schemas.DoctorCreate, db: db_dependency):
-    db_doctor = models.Doctor(**doctor.model_dump())
-    db.add(db_doctor)
-    db.commit()
-    return db_doctor
-
-
-@app.get("/doctors/{doctor_id}", status_code=status.HTTP_200_OK)
-async def read_doctor(doctor_id: int, db: db_dependency):
-    doctor = db.query(models.Doctor).filter(models.Doctor.id == doctor_id).first()
-    if doctor is None:
-        raise HTTPException(status_code=404, detail="Doctor not found")
-    return doctor
-
-
-@app.get("/doctors", status_code=status.HTTP_200_OK)
-async def read_all_doctors(db: db_dependency, skip: int = 0, limit: int = 100):
-    doctors = db.query(models.Doctor).offset(skip).limit(limit).all()
-    return doctors
-
-
-@app.patch("/doctors/{doctor_id}")
-async def update_doctor(doctor_id: int, doctor: schemas.DoctorEdit, db: db_dependency):
-    db_doctor = db.get(models.Doctor, doctor_id)
-    if not db_doctor:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    doctor_data = doctor.model_dump(exclude_unset=True)
-    for key, value in doctor_data.items():
-        setattr(db_doctor, key, value)
-    db.commit()
-    db.refresh(db_doctor)
-    return db_doctor
-
-
-@app.delete("/doctors/{doctor_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_doctor(doctor_id: int, db: db_dependency):
-    # Check if the user exists
-    db_doctor = db.query(models.Doctor).filter(models.Doctor.id == doctor_id).first()
-    if db_doctor is None:
-        raise HTTPException(status_code=404, detail="Doctor not found")
-
-    schedules_to_remove = (
-        db.query(models.Schedule)
-        .filter(models.Schedule.doctor_id == db_doctor.id)
-        .all()
-    )
-    for schedule in schedules_to_remove:
-        await delete_schedule(schedule_id=schedule.id, db=db)
-    try:
-        # db.commit()
-        db.delete(db_doctor)
-        db.commit()
-        return None
-    except StaleDataError:
-        raise HTTPException(
-            status_code=409, detail="Data has changed. Please refresh and try again."
-        )
-
-
-def create_visits(schedule: schemas.ScheduleCreate):
-    start_datetime = datetime.combine(schedule.day, schedule.start)
-    finish_datetime = datetime.combine(schedule.day, schedule.finish)
-    current_datetime = start_datetime
-
-    visits = []
-    while current_datetime < finish_datetime:
-        visit = schemas.VisitCreate(
-            visit_date=current_datetime,
-            description=None,
-            user_id=None,
-            doctor_id=schedule.doctor_id,
-            is_reserved=False,
-        )
-        visits.append(visit)
-        current_datetime += timedelta(minutes=15)
-
-    return visits
-
-
-@app.post(
-    "/schedules",
-    status_code=status.HTTP_201_CREATED,
-    response_model=schemas.ScheduleBase,
-)
-async def schedule_create(schedule: schemas.ScheduleCreate, db: db_dependency):
-    db_schedule = models.Schedule(**schedule.model_dump())
-    db.add(db_schedule)
-    new_visits = create_visits(schedule)
-    for visit in new_visits:
-        db_visit = models.Visit(**visit.model_dump())
-        db.add(db_visit)
-    db.commit()
-    return db_schedule
-
-
-@app.get("/schedules/{schedule_id}", status_code=status.HTTP_200_OK)
-async def read_schedule(schedule_id: int, db: db_dependency):
-    schedule = (
-        db.query(models.Schedule).filter(models.Schedule.id == schedule_id).first()
-    )
-    if schedule is None:
-        raise HTTPException(status_code=404, detail="Doctor not found")
-    return schedule
-
-
-@app.get("/schedules", status_code=status.HTTP_200_OK)
-async def read_all_schedules(db: db_dependency, skip: int = 0, limit: int = 100):
-    schedules = (
-        db.query(models.Schedule)
-        .offset(skip)
-        .limit(limit)
-        .options(joinedload(models.Schedule.doctor))
-        .all()
-    )
-    return schedules
-
-
-@app.delete("/schedules/{schedule_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_schedule(schedule_id: int, db: db_dependency):
-    db_schedule = (
-        db.query(models.Schedule).filter(models.Schedule.id == schedule_id).first()
-    )
-    if db_schedule is None:
-        raise HTTPException(status_code=404, detail="Schedule not found")
-
-    visits_to_remove = (
-        db.query(models.Visit)
-        .filter(models.Visit.doctor_id == db_schedule.doctor_id)
-        .filter(func.date(models.Visit.visit_date) == db_schedule.day)
-        .filter(func.time(models.Visit.visit_date) >= db_schedule.start)
-        .filter(func.time(models.Visit.visit_date) <= db_schedule.finish)
-        .all()
-    )
-    # NOTE: should add here remove_visit function, will do
-    for visit in visits_to_remove:
-        try:
-            db.delete(visit)
-            db.commit()
-        except:
-            raise HTTPException(
-                status_code=409,
-                detail="Data has changed. Please refresh and try again.",
-            )
-    try:
-        db.commit()
-        db.delete(db_schedule)
-        db.commit()
-        return None
-    except StaleDataError:
-        raise HTTPException(
-            status_code=409, detail="Data has changed. Please refresh and try again."
-        )
-
-
-@app.post(
-    "/visits",
-    status_code=status.HTTP_201_CREATED,
-    response_model=schemas.VisitBase,
-)
-async def visit_create(visit: schemas.VisitCreate, db: db_dependency):
-    db_visit = models.Visit(**visit.model_dump())
-    db.add(db_visit)
-    db.commit()
-    return db_visit
-
-
-@app.put("/visits/{visit_id}", response_model=schemas.VisitBase)
-async def edit_visit(visit_id: int, user_id: int, db: db_dependency):
-    db_visit = db.query(models.Visit).filter(models.Visit.id == visit_id).first()
-    if db_visit is None:
-        raise HTTPException(status_code=404, detail="Visit not found")
-
-    db.query(models.Visit).filter(models.Visit.id == visit_id).update(
-        {"user_id": user_id, "is_reserved": True}
-    )
-    db.commit()
-    db.refresh(db_visit)
-
-    return db_visit
-
-
-@app.put(
-    "/visits/cancel/{visit_id}/{visit_row_version}", response_model=schemas.VisitBase
-)
-async def edit_visit_reservation(
-    visit_id: int, visit_row_version: int, db: db_dependency
-):
-    db_visit = db.query(models.Visit).filter(models.Visit.id == visit_id).first()
-    if db_visit is None:
-        raise HTTPException(status_code=404, detail="Visit not found")
-    if db_visit.row_version != visit_row_version:
-        raise HTTPException(status_code=409, detail="Data has changed")
-
-    db_visit.user_id = None
-    db_visit.is_reserved = False
-    db_visit.description = None
-
-    db.commit()
-    db.refresh(db_visit)
-    try:
-        db.commit()
-        db.refresh(db_visit)
-        return db_visit
-    except StaleDataError:
-        raise HTTPException(status_code=409, detail="Somethign went wrong")
-
-
-@app.patch("/visits/{visit_id}", response_model=schemas.VisitBase)
-async def edit_visit_description(
-    visit_id: int, visit: schemas.VisitUpdate, db: db_dependency
-):
-    db_visit = db.query(models.Visit).filter(models.Visit.id == visit_id).first()
-    if db_visit is None:
-        raise HTTPException(status_code=404, detail="Visit not found")
-    if db_visit.row_version != visit.row_version:
-        raise HTTPException(status_code=409, detail="Data has changed")
-
-    update_data = visit.model_dump(exclude_unset=True)
-    for key, value in update_data.items():
-        setattr(db_visit, key, value)
-
-    try:
-        db.commit()
-        db.refresh(db_visit)
-        return db_visit
-    except StaleDataError:
-        raise HTTPException(
-            status_code=409, detail="Data has changed. Please refresh and try again."
-        )
-
-
-@app.get("/visits", status_code=status.HTTP_200_OK)
-async def read_all_visits(db: db_dependency, skip: int = 0, limit: int = 100):
-    visits = (
-        db.query(models.Visit)
-        .offset(skip)
-        .limit(limit)
-        .options(joinedload(models.Visit.user), joinedload(models.Visit.doctor))
-        .all()
-    )
-    return visits
-
-
-@app.get("/specialities", status_code=status.HTTP_200_OK)
-async def get_specialitis():
-    specialties = [specialty for specialty in models.Speciality]
-    return specialties
+@app.route("/{path:path}")
+async def read_root(request: Request, path: str = None):
+    return templates.TemplateResponse("index.html", {"request": request})
